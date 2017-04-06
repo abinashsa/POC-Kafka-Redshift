6f8f780c555442facc32aa107322dbca0f84a68b
Abinash Sahoo <Abinash Sahoo@DESKTOP-HVI0RRQ.fios-router.home>
2017-04-05 22:17:33 -0400

    framework

---
+++ src/main/java/com/aws/poc/AwsRedshiftKafkaPocApplication.java
@@ -0,0 +1,12 @@
+package com.aws.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRedshiftKafkaPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsRedshiftKafkaPocApplication.class, args);
+	}
+}
